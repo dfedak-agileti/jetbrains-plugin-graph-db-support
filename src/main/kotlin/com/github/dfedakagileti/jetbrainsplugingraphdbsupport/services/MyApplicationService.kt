5562666df81f3bdf4964463e1cdd3b20d594b29d
GitHub Action <action@github.com>
2022-07-10 15:08:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dfedakagileti/jetbrainsplugingraphdbsupport/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dfedakagileti.jetbrainsplugingraphdbsupport.services
+
+import com.github.dfedakagileti.jetbrainsplugingraphdbsupport.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dfedakagileti/jetbrainsplugingraphdbsupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dfedakagileti/jetbrainsplugingraphdbsupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dfedakagileti.jetbrainsplugingraphdbsupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dfedakagileti.jetbrainsplugingraphdbsupport.MyBundle
 
 class MyProjectService(project: Project) {
 
